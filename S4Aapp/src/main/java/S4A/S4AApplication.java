63072f7664943710e848f5112b59640a15ba0f3c
Mikolaj Prokop <prok.mikolaj@gmail.com>
2022-07-01 21:52:42 +0200

    Minor changes

--- S4Aapp/src/main/java/S4A/S4AApplication.java
+++ S4Aapp/src/main/java/S4A/S4AApplication.java
@@ -2,13 +2,6 @@ package S4A;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.Collections;
 
 @SpringBootApplication
 public class S4AApplication {
